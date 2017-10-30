2e4076d8d67875d27bda8b53b2ab0a3447f7405f
Unknown <kleynenbo@gmail.com>
2017-10-30 22:49:52 +0100

    (finally) found a way to use relative paths

--- src/Util.py
+++ src/Util.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import datetime, date, time
 
 
-average_wind_speed_path = "/Users/bokleynen/Documents/2Bir/P&O3/Smart-Energy-Home/data/Average-Wind-Speed.csv"
+average_wind_speed_path = "../data/Average-Wind-Speed.csv"
 average_wind_speed_ds = pd.read_csv(average_wind_speed_path,
                                     index_col="Date/Time",
                                     converters={"miles-per-hour": lambda x: 0.44704 * float(x) if x != "" else np.nan},
@@ -12,7 +12,7 @@ average_wind_speed_ds = pd.read_csv(average_wind_speed_path,
                                     infer_datetime_format=True
                                     )
 
-irradiance_path = "/Users/bokleynen/Documents/2Bir/P&O3/Smart-Energy-Home/data/Irradiance.csv"
+irradiance_path = "../data/Irradiance.csv"
 irradiance_ds = pd.read_csv(irradiance_path,
                             index_col="Date/Time",
                             skiprows=8,
