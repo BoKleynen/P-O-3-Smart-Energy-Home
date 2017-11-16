430e672500f2c2cb6105bf65e1b89b2656c4940c
Unknown <kleynenbo@gmail.com>
2017-11-16 17:02:37 +0100

    updated house

--- src/house/House.py
+++ src/house/House.py
@@ -6,7 +6,9 @@ import math
 from house.Loads import Load, StaggeredLoad, TimedLoad, ContinuousLoad
 from house.production.SolarPanel import SolarPanel
 from house.production.WindMill import Windmill
+from house.Battery import Battery
 from typing import Iterable, List
+from datetime import datetime, date, time, timedelta
 
 
 DAY_SECONDS = 86400
@@ -19,7 +21,7 @@ class House:
     """
 
     def __init__(self, load_it: Iterable[Load], solar_panel: SolarPanel=None, nb_solar_panel: int=0,
-                 windmill: Windmill=None, nb_windmill: int=0):
+                 windmill: Windmill=None, nb_windmill: int=0, battery: Battery=None):
         """
 
         :param load_it: An iterable containing all loads in the house
@@ -35,7 +37,12 @@ class House:
         self.nb_solar_panel: int = nb_solar_panel if self.solar_panel is not None else 0
         self.windmill: Windmill = windmill
         self.nb_windmill: int = nb_windmill if self.windmill is not None else 0
+        self.battery = battery
         self.is_optimised = False
+        battery.house = self
+        self.is_large_installation = self.nb_solar_panel * self.solar_panel.peak_power if self.has_solar_panel() else 0\
+            + self.nb_windmill * self.windmill.power(self.windmill.max_wind_speed) if self.has_windmill() else 0 > 10000.0
+        self.coordinates = None
 
     def has_windmill(self) -> bool:
         """
@@ -58,7 +65,7 @@ class House:
         """
         return math.fsum(
             map(
-                lambda load: load.power_consumption,
+                lambda load: load.power,
                 self.continuous_load_list
             )
         )
@@ -88,10 +95,13 @@ class House:
         """
         return math.fsum(
             map(
-                lambda load: load.power_consumption(t),
+                lambda load: load.power(t),
                 self.timed_load_list
             )
         )
+
+    def total_load_power(self, t: float, t_arr: np.ndarray):
+        return self.continuous_load_power() + self.timed_load_power(t) + self.staggered_load_power(t, t_arr)
 
     def produced_own_power(self, t: float) -> float:
         """
@@ -99,18 +109,41 @@ class House:
         :param t:
         :return: The total amount of power produced by the house at a given time
         """
-        return self.windmill.power_production(t) if self.has_windmill() else 0 \
-            + self.solar_panel.power_production(t) if self.has_solar_panel() else 0
+        return self.windmill.power(t) if self.has_windmill() else 0 \
+            + self.solar_panel.power(t) if self.has_solar_panel() else 0
 
-    def available_own_power(self, t: float) -> float:
+    def total_power_consumption(self, t, t_arr):
+        return self.total_load_power(t, t_arr) - self.produced_own_power(t)
+
+    def _staggered_cost(self, t_arr: np.ndarray) -> float:
         """
 
-        :param t:
-        :return: The amount of self produced power available for staggered loads at the given time t
+        :param t_arr:
+        :return:
         """
-        return max(self.produced_own_power(t) - self.timed_load_power(t), 0.0)
+        if len(t_arr) != len(self.staggered_load_list):
+            raise Exception("iterable length mismatch")
 
-    # TODO: add support for some kind of database object, storing needed data like electricity price, wind speed, ...
+        t = [t for t in range(0, DAY_SECONDS, 300)] \
+            + [load.start_time for load in self.timed_load_list] \
+            + [load.start_time + load.cycle_duration for load in self.timed_load_list] \
+            + [t_arr[i] for i in range(len(t_arr))] \
+            + [t_arr[i] + self.staggered_load_list[i].cycle_duration for i in range(len(t_arr))]
+
+        t.sort()
+
+        total_used_energy = 0.0
+        cost = 0.0
+
+        for i in range(len(t)):
+            time_delta = t[i + 1] - t[i]
+            total_power = self.total_power_consumption(t, t_arr)
+            used_energy = time_delta * (total_power + self.battery.power(time_delta, total_power))
+            total_used_energy += used_energy
+            cost += self.electricity_cost(t[i], used_energy)
+
+        return cost
+
     def optimise(self) -> List[float]:
         """
         Computes the optimal timings for the staggered loads to start their job.
@@ -118,27 +151,12 @@ class House:
         :return: A list containing the optimal start times for the staggered loads in this house
         """
 
-        def staggered_cost(t_arr: np.ndarray) -> float:
-            """
-
-            :param t_arr:
-            :return:
-            """
-            if len(t_arr) != len(self.staggered_load_list):
-                raise Exception("iterable length mismatch")
-
-            return integrate.quad(
-                lambda t: max(
-                    (self.staggered_load_power(t, t_arr) - self.available_own_power(t)) * price(t),
-                    0.0),
-                0.0, DAY_SECONDS)[0]
-
         init_guesses = np.array([random.random() * DAY_SECONDS for i in range(len(self.staggered_load_list))])
 
         cons = ({'type': 'ineq', 'fun': lambda t: DAY_SECONDS - (t[i] + self.staggered_load_list[i])}
                 for i in range(len(self.staggered_load_list)))
 
-        res = opt.minimize(staggered_cost, init_guesses, constraints=cons)
+        res = opt.minimize(self._staggered_cost, init_guesses, constraints=cons)
 
         return res.x
 
@@ -155,12 +173,26 @@ class House:
         for i in range(len(self.staggered_load_list)):
             self.staggered_load_list[i].start_time = t_it[i]
 
+    def electricity_cost(self, t: datetime, consumed_energy) -> float:
+        """
 
-def price(t):
-    """
-    temporary function
+        :param t:
+        :param consumed_energy: energy in kWh
+        :return:
 
-    :param t:
-    :return:
-    """
-    return 20 if 28800 < t <= 72000 else 10
+            peak:       8:00 - 20:00 on weekdays
+            off-peak:   20:00 - 8:00 and weekends
+        """
+        if self.is_large_installation:
+            if consumed_energy >= 0:
+                return 0.24 * consumed_energy
+
+            else:
+                if t.time() < time(hour=8) or t.time() > time(hour=20) or t.isoweekday() >= 6:
+                    return 0.036 * consumed_energy
+
+                else:
+                    return 0.052 * consumed_energy
+
+        else:
+            return 0.24 * consumed_energy
