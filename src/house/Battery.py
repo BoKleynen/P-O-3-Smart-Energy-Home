7ca6b45c718afd82dcfab2db6e962ada32baa346
Unknown <kleynenbo@gmail.com>
2017-11-16 17:02:11 +0100

    updated battery

--- src/house/Battery.py
+++ src/house/Battery.py
@@ -3,16 +3,20 @@ class Battery:
     a class modelling a batter that can be used to store locally produced energy
     """
 
-    def __init__(self, voltage: float, capacity: float, charge: float=0):
-        self.voltage: float = voltage
+    def __init__(self, capacity: float, max_power: float, stored_energy: float=0.0):
         self.capacity: float = capacity
-        self.charge: float = charge
-        
-    def get_voltage(self) -> float:
-        return self.voltage
-    
-    def get_capacity(self) -> float:
-        return self.capacity
+        self.max_power: float = max_power
+        self.stored_energy: float = stored_energy
 
-    def get_charge(self) -> float:
-        return self.charge
+    def power(self, time_delta, needed_power):
+        if needed_power > 0:
+            if self.stored_energy - time_delta * min(needed_power, self.max_power) >= 0:
+                self.stored_energy -= time_delta * min(needed_power, self.max_power)
+                return min(needed_power, self.max_power)
+
+            elif self.stored_energy - time_delta * min(self.stored_energy/time_delta, self.max_power):
+                self.stored_energy -= time_delta * min(self.stored_energy/time_delta, self.max_power)
+                return min(self.stored_energy/time_delta, self.max_power)
+
+        else:
+            return max(needed_power, -self.max_power)
