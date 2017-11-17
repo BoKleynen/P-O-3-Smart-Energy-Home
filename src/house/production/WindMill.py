8b28c3262f1e8d177c0568034eed214fd386d8e0
Bo Kleynen <kleynenbo@gmail.com>
2017-11-17 12:48:08 +0100

    Windmills now have an associated house

--- src/house/production/WindMill.py
+++ src/house/production/WindMill.py
@@ -6,9 +6,14 @@ class Windmill:
         self.radius = radius
         self.min_wind_speed = min_wind_speed
         self.max_wind_speed = max_wind_speed
+        self._house = None
+
+    @property
+    def house(self):
+        return self._house
 
     @staticmethod
-    def __power_production(wind_speed: float, area: float) -> float:
+    def _power_production(wind_speed: float, area: float) -> float:
         """
 
         :param wind_speed:
@@ -27,7 +32,7 @@ class Windmill:
             return 0
 
         elif wind_speed < self.max_wind_speed:
-            return self.__power_production(wind_speed, math.pi * math.pow(self.radius, 2))
+            return self._power_production(wind_speed, math.pi * math.pow(self.radius, 2))
 
         else:
-            return self.__power_production(self.max_wind_speed, math.pi * math.pow(self.radius, 2))
+            return self._power_production(self.max_wind_speed, math.pi * math.pow(self.radius, 2))
